bc47c908abcf78f4385cc139fa9730917a452b66
chrnk-exe <ivan_kot2001@mail.ru>
2022-11-17 19:41:18 +0300

    mainpagefix

--- client/src/globals.d.ts
+++ client/src/globals.d.ts
@@ -6,3 +6,11 @@ declare interface BinGCDInput {
 declare interface BinGCDOutput { 
     result: string
 }
+
+type matrix = Array<Array<number>>
+
+declare interface APIResponse {
+    label: string
+    data: string | Array<number> | matrix
+    type: typeof APIResponse.data
+}
